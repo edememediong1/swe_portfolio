87f76e02fa96a2846d604132aa0af0beb8a4371a
Emediong Bassey Edem <edememediong1@gmail.com>
2026-01-13 22:59:58 +0100

    created the Testimonial section and added the SectionHeader component

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -17,7 +17,7 @@ const words = [
 export const TapeSection = () => {
   return <section className="py-16 lg:py-24 overflow-x-clip">
     <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1">
-      <div className="flex" style={{maskImage: 'linear-gradient(to_right,transparent,black_10%, black_90%, transparent'}}>
+      <div className="flex" style={{maskImage: 'linear-gradient(to right, transparent, black 10%, black 90%, transparent)'}}>
         <div className="flex flex-none gap-4 py-3">
           { words.map((word) => {
             return ( 
