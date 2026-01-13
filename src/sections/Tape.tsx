9245b5c93267811bf1e75e97630bfdc6373c8d78
Emediong Bassey Edem <edememediong1@gmail.com>
2026-01-13 22:07:14 +0100

    added tape section #1

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -15,14 +15,16 @@ const words = [
 
 export const TapeSection = () => {
   return <section className="py-16">
-    <div className="bg-gradient-to-r from-emerald-300 to-sky-400">
-      <div className="">
-        <div className="flex gap-4">
+    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1">
+      <div className="flex" style={{maskImage: 'linear-gradient(to_right,transparent,black_10%, black_90%, transparent'}}>
+        <div className="flex flex-none gap-4 py-3">
           { words.map((word) => {
             return ( 
-            <div key={word} className="inline-flex gap-4">
-              <span>{word}</span>
-              <StarIcon/>
+            <div key={word} className="inline-flex gap-4 items-center">
+              <span className="text-gray-900 uppercase font-extrabold text-sm">
+                {word}
+              </span>
+              <StarIcon className="size-6 text-gray-900 -rotate-12"/>
             </div>
             )})}
         </div>
